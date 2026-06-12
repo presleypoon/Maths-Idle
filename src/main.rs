d38d09bf5b5a8d13becbaa40e36b67a889ce5d45
Presley <presleypoon37@gmail.com>
2026-06-12 17:27:06 +0100

    Auto-Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use terminal_size::{Width, terminal_size};
+
 struct Theory {
     id: u8,
     name: String,
@@ -7,14 +9,15 @@ struct Theory {
     cost: u128,
     unlock_critia: Vec<u8>,
     check: Vec<u8>,
-    unlocked: bool,
+    unlocked: bool, 
     ppt: u128,
     shown: bool,
 }
 
 struct GameState {
     point: u128,
-    worker_no: u32,
+    worker: u8,
+    total_pps: u128,
 }
 
 fn main() -> () {
@@ -42,12 +45,56 @@ fn main() -> () {
             shown: true,
         },
     ];
-    
-    render(&mut theories);
+
+    let /* mut */ game_state: GameState = GameState{
+        point: 0,
+        worker: 0,
+        total_pps: 0,
+    };
+
+    let width: usize = terminal_size()
+        .map(|(Width(w), _)| w as usize)
+        .unwrap_or(80);
+
+    render(
+        &mut theories,
+        width,
+        game_state.point,
+        game_state.total_pps,
+        game_state.worker,
+    );
 }
 
-fn render(theories: &mut Vec<Theory>) {
+fn render(
+    theories: &mut Vec<Theory>,
+    width: usize,
+    point: u128,
+    pps: u128,
+    workers: u8,
+) -> () {
+    let rounded_point: String = point.to_string();
+
+    println!(
+        "Score: {:>6}    PPS: {:>6}    Worker(s): {:>3}",
+        rounded_point, pps, workers
+    );
+    println!("{}", "=".repeat(width));
+
     for theory in theories {
-        
+        if !theory.shown {
+            continue;
+        }
+
+        println!(
+            "{:>3}. {:.<24} Point: {:.<5}.....{}",
+            theory.id,
+            theory.name,
+            theory.cost,
+            if theory.unlocked {
+                "Unlocked"
+            } else {
+                "Locked"
+            },
+        );
     }
 }
