ebf559cf0c02d39a8f160174d1df1e74c5adda26
Presley <presleypoon37@gmail.com>
2026-06-13 08:21:02 +0100

    Auto-Update

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use crossterm::cursor::MoveTo;
 use crossterm::terminal::{Clear, ClearType};
 use std::io::{Write, stdin, stdout};
 use std::ops::Index;
+use std::process::ExitStatus;
 use terminal_size::{Width, terminal_size};
 
 struct Theory {
@@ -50,7 +51,7 @@ fn main() -> () {
         },
     ];
 
-    let mut game_state: GameState = GameState{
+    let mut game_state: GameState = GameState {
         point: 0,
         worker: 0,
         total_pps: 0,
@@ -75,16 +76,21 @@ fn main() -> () {
         let input: String = get_user_input("What do you want to unlock");
         let mut input_index: Option<usize> = None;
         for (i, theory) in theories.iter_mut().enumerate() {
-            if theory.name.to_lowercase() == input.to_lowercase() && theory.shown && !theory.unlocked && game_state.point >= theory.cost {
+            if theory.name.to_lowercase() == input.to_lowercase()
+                && theory.shown
+                && !theory.unlocked
+                && game_state.point >= theory.cost
+            {
                 input_index = Some(i);
                 game_state.point -= theory.cost;
             }
         }
-
         if let Some(i) = input_index {
             theories[i].unlocked = true;
         }
 
+        // let _ = get_user_input("Testing").parse().unwrap();
+        
         let _ = write!(stdout(), "{}", MoveTo(0, 0));
         let _ = write!(stdout(), "{}", Clear(ClearType::All));
 
@@ -99,11 +105,12 @@ fn main() -> () {
 }
 
 fn render(theories: &mut Vec<Theory>, width: usize, point: u128, pps: u128, worker: u8) -> () {
-    let rounded_point: String = point.to_string();
+    let ign_point: String = number_to_ign(point);
+    let ign_pps: String = number_to_ign(pps);
 
     println!(
         "Score: {:>6}    PPS: {:>6}    Worker(s): {:>3}",
-        rounded_point, pps, worker
+        ign_point, ign_pps, worker
     );
     println!("{}", "=".repeat(width));
 
@@ -127,7 +134,7 @@ fn render(theories: &mut Vec<Theory>, width: usize, point: u128, pps: u128, work
 }
 
 fn get_user_input(prompt: &str) -> String {
-    print!("{}", prompt);
+    print!("{}: ", prompt);
 
     let _ = stdout().flush();
 
@@ -136,4 +143,27 @@ fn get_user_input(prompt: &str) -> String {
     stdin().read_line(&mut input).expect("Failed to read line");
 
     input.trim().to_string()
+}
+
+fn number_to_ign(number: u128) -> String {
+    if number < 1000 {
+        return format!("{:>4}  ", number);
+    }
+
+    let suffixes: [&str; 12] = [
+        "K ", "M ", "B ", "T ", "Qa", "Qi", "Sx", "Sp", "O ", "N ", "D ", "U ",
+    ];
+
+    let f_num: f32 = number as f32;
+    let exp_i_div_3: usize = (f_num.log10() / 3.0) as usize;
+    let coef: f32 = f_num / 1000_f32.powi(exp_i_div_3 as i32);
+    let suffix: &str = suffixes[exp_i_div_3 - 1];
+
+    return if coef >= 100.0 {
+        format!("{:>4.0}{}", coef, suffix)
+    } else if coef >= 10.0 {
+        format!("{:>4.1}{}", coef, suffix)
+    } else {
+        format!("{:>4.2}{}", coef, suffix)
+    };
 }
